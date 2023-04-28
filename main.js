4a27e87c4cd44323983e68bff49bba177037c63c
Your NameWaigy666 <ch20217@students.nulondon.ac.uk>
2023-04-28 14:23:16 +0100

    readme-evaluation

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-const scoreHistory = [];
+const scoreHistory = []; //set score history to null
 
-const MINIMUM_MS_TILL_CHANGE = 3000;
-const MAXIMUM_MS_TILL_CHANGE = 10000;
+const MINIMUM_MS_TILL_CHANGE = 3000; // set the min ms until screen colour change
+const MAXIMUM_MS_TILL_CHANGE = 10000; // set the maximum ms until screen colour change
 
-let msSinceEpochOnTimeout = 0;
-let waitingForClick = false;
+let msSinceEpochOnTimeout = 0; // set the timer to 0
+let waitingForClick = false; // set the eventlistener waitingforclick to false, true is returned when click received
 
 function play() {
+  // generate random number between 0 and 1. and multiply it by a value between the min and max ms until change
   const msTillChange =
     Math.floor(
-      Math.random() * (MAXIMUM_MS_TILL_CHANGE - MINIMUM_MS_TILL_CHANGE)
-    ) + MINIMUM_MS_TILL_CHANGE;
+      Math.random() * (MAXIMUM_MS_TILL_CHANGE - MINIMUM_MS_TILL_CHANGE) // generate random number from 1 to 10000
+    ) + MINIMUM_MS_TILL_CHANGE; // ensure random number is greater than min
 
-  // Revert the colour back to red
+  // Default to CSS defined background colour
   clickArea.style.backgroundColor = null;
 
   displayText.textContent = "";
 
   setTimeout(() => {
-    msSinceEpochOnTimeout = Date.now();
+    msSinceEpochOnTimeout = Date.now(); //set timer to start
 
-    clickArea.style.backgroundColor = "#e0680d";
-    waitingForClick = true;
-  }, msTillChange);
+    clickArea.style.backgroundColor = "#e0680d"; //change background colour
+    waitingForClick = true; // record time click received
+  }, msTillChange); 
 }
 
 function addScore(score) {
   // add score to array in index 0
   scoreHistory.unshift(score);
-
+  // create for loop to populate the score boxes restarting at 5
   for (let i = 0; i < Math.min(scoreHistory.length, 5); i++) {
     const score = scoreHistory[i];
 
@@ -36,14 +37,15 @@ function addScore(score) {
   }
 }
 
-
 function scorefeedback(score) {
+  // set constants for the returned message
 scoretext =  'Your time was ' + score + ' ms!\r\n';
 agerange = getagerange(score)
 displayText.setAttribute('style', 'white-space: pre;');
 displayText.textContent = scoretext + "\r\n" + agerange + "\r\n\r\nClick to play again.";
 }
 
+// defines the score ranges and retunred message
 function getagerange(score) {
   switch (true) {
   case  score <= 298:
@@ -65,4 +67,4 @@ function getagerange(score) {
   }
 }
 
-module.exports = { getagerange }
+module.exports = { getagerange } // enable npm testing
